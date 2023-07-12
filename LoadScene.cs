0b55fa9c087f19e79646e9ffc442e5273ff5226a
iwanami <41913491+aozorakyozai@users.noreply.github.com>
2023-07-13 04:42:55 +0900

    Add files via upload

--- LoadScene.cs
+++ LoadScene.cs
@@ -1,14 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
 /********************************************************************
  * 最終更新：2023/04/07
  * 環境：Unity 2023.3.13f1 LTS
  * 環境：Unity C# 
  * 機能：シーンの変更
 *********************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーン用
+
 public class LoadScene : MonoBehaviour
 {
     GameManager gameManager = new GameManager();
@@ -19,9 +20,9 @@ public class LoadScene : MonoBehaviour
     }
     public void OnClick3DSUDOKU()
     {
-        // 保存
+        // 回答numberの保存
         gameManager.SaveNumberAnswer();
-
+        // 回転角度の保存
         gameManager.SaveCubeAngle();
 
         SceneManager.LoadScene("Setting");
